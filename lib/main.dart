a0ab7fceef16053c9026ab93149bd92809b02b41
Ahmed Abdo <77578718+eng-ahmedabdo@users.noreply.github.com>
2025-10-31 23:16:07 +0200

    Update main file
    - remove debugModeBanner
    - change appar title

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Welcome to Flutter'),
     );
   }
 }
